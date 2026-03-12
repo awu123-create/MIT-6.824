ffe4d18b139d9c0ee911bcc20b84d1c3da5048ee
awu123-create <zzy060103@gmail.com>
2026-03-12 17:01:13 +0800

    finish lab3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,18 @@
 package kvraft
 
-import "../labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
 
+	"../labrpc"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leaderID  int   // 用于记录当前认为的Leader的ID，减少发送请求时重试的次数
+	clientID  int64 // 唯一标识一个客户端
+	requestID int64 // 每个请求的唯一ID，递增生成，配合clientID可以唯一标识一个请求，防止重复执行同一请求
 }
 
 func nrand() int64 {
@@ -21,10 +26,11 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+
+	ck.clientID = nrand()
 	return ck
 }
 
-//
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
@@ -35,14 +41,38 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) Get(key string) string {
+	ck.requestID++ // 生成新的请求ID
+	args := &GetArgs{
+		Key:       key,
+		ClientID:  ck.clientID,
+		RequestID: ck.requestID,
+	}
+
+	// 首先尝试发送请求到当前认为的Leader服务器，如果请求失败或者返回错误，则轮询其他服务器，直到成功为止
+	for {
+		reply := &GetReply{}
+		ok := ck.servers[ck.leaderID].Call("KVServer.Get", args, reply)
+		if ok {
+			switch reply.Err {
+			case OK:
+				return reply.Value
+			case ErrNoKey:
+				return ""
+			default:
+
+			}
+
+		}
+
+		// 请求失败或者返回错误，尝试下一个服务器
+		ck.leaderID = (ck.leaderID + 1) % len(ck.servers)
+	}
 
 	// You will have to modify this function.
-	return ""
+
 }
 
-//
 // shared by Put and Append.
 //
 // you can send an RPC with code like this:
@@ -51,9 +81,27 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	ck.requestID++
+	args := &PutAppendArgs{
+		Key:       key,
+		Value:     value,
+		Op:        op,
+		ClientID:  ck.clientID,
+		RequestID: ck.requestID,
+	}
+
+	for {
+		reply := &PutAppendReply{}
+		ok := ck.servers[ck.leaderID].Call("KVServer.PutAppend", args, reply)
+		if ok && reply.Err == OK {
+			return
+		}
+
+		// 请求失败或者返回错误，尝试下一个服务器
+		ck.leaderID = (ck.leaderID + 1) % len(ck.servers)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -1,12 +1,14 @@
 package kvraft
 
 import (
-	"../labgob"
-	"../labrpc"
 	"log"
-	"../raft"
 	"sync"
 	"sync/atomic"
+	"time"
+
+	"../labgob"
+	"../labrpc"
+	"../raft"
 )
 
 const Debug = 0
@@ -18,11 +20,28 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+
+	OpType    string // "Get", "Put" or "Append"
+	Key       string
+	Value     string
+	ClientID  int64 // 每个客户端一个唯一ID
+	RequestID int64 // 每个请求一个唯一ID，客户端递增
+}
+
+type LastOp struct {
+	RequestID int64    // 上一次请求的RequestID
+	Result    OpResult // 上一次请求的结果
+}
+
+type OpResult struct {
+	Err       Err    // Raft处理客户端命令时产生的错误
+	Value     string // 客户端Get请求读取的值
+	ClientID  int64  // 和RequestID一起唯一标识一个请求，用于去重
+	RequestID int64
 }
 
 type KVServer struct {
@@ -35,18 +54,173 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	kvDB        map[string]string     // kv存储
+	lastRequest map[int64]LastOp      // 每个客户端的上一次请求结果，key是ClientID
+	notifyChs   map[int]chan OpResult // 每个日志索引对应一个通知通道，Raft apply一个日志后，通过这个通道通知等待的请求处理结果
 }
 
-
+// 客户端通过Clerk发送请求，然后KVServer通过Raft共识算法达成日志一致，最后KVServer apply日志并执行对应的操作。每个请求都包含ClientID和RequestID，用于去重和返回结果。
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	// 1. 构造Op
+	op := Op{
+		OpType:    "Get",
+		Key:       args.Key,
+		ClientID:  args.ClientID,
+		RequestID: args.RequestID,
+	}
+
+	// 2. 调用Raft的Start()方法将Op发送给Raft
+	index, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	// 3. 等待Raft apply日志并执行对应的操作，使用notifyChs进行通知
+	kv.mu.Lock()
+	ch, ok := kv.notifyChs[index]
+	if !ok {
+		ch = make(chan OpResult, 1)
+		kv.notifyChs[index] = ch
+	}
+	kv.mu.Unlock()
+
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.notifyChs, index)
+		kv.mu.Unlock()
+	}()
+
+	select {
+	case result := <-ch:
+		// 进入此分支说明Raft已经apply了这个日志并执行了对应的操作，result是执行结果
+		if result.ClientID == args.ClientID && result.RequestID == args.RequestID {
+			reply.Err = result.Err
+			reply.Value = result.Value
+		} else {
+			// 进入此分支有两种情况：
+			// 1. 此请求之前被处理过了
+			// 2. 服务器在调用Start方法时还是Leader，但当初那个 entry 还没提交，它所在的 log 位置后来可能被新 leader 的别的命令替换
+			reply.Err = ErrWrongLeader
+		}
+
+	case <-time.After(100 * time.Millisecond):
+		reply.Err = ErrTimeout
+	}
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	op := Op{
+		OpType:    args.Op,
+		Key:       args.Key,
+		Value:     args.Value,
+		ClientID:  args.ClientID,
+		RequestID: args.RequestID,
+	}
+
+	index, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	kv.mu.Lock()
+	ch, ok := kv.notifyChs[index]
+	if !ok {
+		ch = make(chan OpResult, 1)
+		kv.notifyChs[index] = ch
+	}
+	kv.mu.Unlock()
+
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.notifyChs, index)
+		kv.mu.Unlock()
+	}()
+
+	select {
+	case result := <-ch:
+		if result.ClientID == args.ClientID && result.RequestID == args.RequestID {
+			reply.Err = result.Err
+		} else {
+			reply.Err = ErrWrongLeader
+		}
+
+	case <-time.After(100 * time.Millisecond):
+		reply.Err = ErrTimeout
+	}
+
 }
 
-//
+func (kv *KVServer) applier() {
+	for !kv.killed() {
+		select {
+		case msg := <-kv.applyCh:
+			kv.apply(msg)
+		case <-time.After(100 * time.Millisecond):
+		}
+	}
+}
+
+func (kv *KVServer) apply(msg raft.ApplyMsg) {
+	if !msg.CommandValid {
+		return
+	}
+
+	op := msg.Command.(Op)
+
+	kv.mu.Lock()
+	// 查重
+	lastOp, ok := kv.lastRequest[op.ClientID]
+	ch, chExist := kv.notifyChs[msg.CommandIndex]
+	kv.mu.Unlock()
+	if ok && op.RequestID <= lastOp.RequestID {
+		// 已经处理过这个请求，直接返回上一次的结果
+		if chExist {
+			ch <- lastOp.Result
+		}
+
+		return
+	}
+
+	// 执行操作（修改kvDB、lastRequest）
+	result := OpResult{
+		ClientID:  op.ClientID,
+		RequestID: op.RequestID,
+	}
+	kv.mu.Lock()
+	switch op.OpType {
+	case "Get":
+		value, ok := kv.kvDB[op.Key]
+		if ok {
+			result.Err = OK
+			result.Value = value
+		} else {
+			result.Err = ErrNoKey
+		}
+	case "Put", "Append":
+		if op.OpType == "Append" {
+			kv.kvDB[op.Key] += op.Value
+		} else {
+			kv.kvDB[op.Key] = op.Value
+		}
+		result.Err = OK
+	}
+
+	kv.lastRequest[op.ClientID] = LastOp{
+		RequestID: op.RequestID,
+		Result:    result,
+	}
+	// ch, ok := kv.notifyChs[msg.CommandIndex]
+	kv.mu.Unlock()
+	// 通过notifyChs通知等待的请求处理结果
+	if chExist {
+		ch <- result
+	}
+}
+
 // the tester calls Kill() when a KVServer instance won't
 // be needed again. for your convenience, we supply
 // code to set rf.dead (without needing a lock),
@@ -55,7 +229,6 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 // code to Kill(). you're not required to do anything
 // about this, but it may be convenient (for example)
 // to suppress debug output from a Kill()ed instance.
-//
 func (kv *KVServer) Kill() {
 	atomic.StoreInt32(&kv.dead, 1)
 	kv.rf.Kill()
@@ -67,7 +240,6 @@ func (kv *KVServer) killed() bool {
 	return z == 1
 }
 
-//
 // servers[] contains the ports of the set of
 // servers that will cooperate via Raft to
 // form the fault-tolerant key/value service.
@@ -80,7 +252,6 @@ func (kv *KVServer) killed() bool {
 // you don't need to snapshot.
 // StartKVServer() must return quickly, so it should start goroutines
 // for any long-running work.
-//
 func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int) *KVServer {
 	// call labgob.Register on structures you want
 	// Go's RPC library to marshall/unmarshall.
@@ -94,8 +265,12 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
+	kv.kvDB = make(map[string]string)
+	kv.lastRequest = make(map[int64]LastOp)
+	kv.notifyChs = make(map[int]chan OpResult)
 
 	// You may need initialization code here.
+	go kv.applier()
 
 	return kv
 }
